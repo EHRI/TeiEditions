203a66f67af418ebdc3bb9f98baae01fed0b5140
Mike Bryant <mikesname@gmail.com>
2018-01-11 16:09:34 +0000

    Initial WIP on Neatline record import from TEI

--- controllers/FilesController.php
+++ controllers/FilesController.php
@@ -47,11 +47,20 @@ class TeiEditions_FilesController extends Omeka_Controller_AbstractActionControl
             'description' => __('A TEI file to upload as a new item'),
             'required' => true
         ));
+
+        $form->addElement('checkbox', 'create_exhibit', array(
+            'id' => 'tei-editions-upload-create-exhibit',
+            'label' => __('Create Neatline Exhibit'),
+            'class' => 'checkbox',
+            'description' => __('Create a Neatline Exhibit containing records for each place element contained in the TEI')
+        ));
+
+
         $form->addElement('submit', 'submit', array(
             'label' => __('Import TEI')
         ));
 
-        $form->addDisplayGroup(array('file'), 'teiimport_info');
+        $form->addDisplayGroup(array('file', 'create_exhibit'), 'teiimport_info');
         $form->addDisplayGroup(array('submit'), 'teiimport_submit');
 
         return $form;
@@ -67,16 +76,50 @@ class TeiEditions_FilesController extends Omeka_Controller_AbstractActionControl
                 $this->_helper->_flashMessenger(__('There was an error on the form. Please try again.'), 'error');
                 return;
             }
-            $item = new Item;
-            $xpaths = TeiEditionsFieldMapping::fieldMappings();
-            $path = $_FILES["file"]["tmp_name"];
-            $data = @tei_editions_extract_metadata($path, $xpaths);
-            error_log("Extracted from " . $path . " -> " .
-                json_encode($data, JSON_PRETTY_PRINT));
 
-            $item->addElementTextsByArray($data);
-            $item->save();
-            @insert_files_for_item($item, "Upload", "file");
+            $tx = get_db()->getAdapter()->beginTransaction();
+            try {
+
+                $item = new Item;
+                $xpaths = TeiEditionsFieldMapping::fieldMappings();
+                $path = $_FILES["file"]["tmp_name"];
+                $data = @tei_editions_extract_metadata($path, $xpaths);
+                error_log("Extracted from " . $path . " -> " .
+                    json_encode($data, JSON_PRETTY_PRINT));
+
+                $item->addElementTextsByArray($data);
+                $item->save();
+                @insert_files_for_item($item, "Upload", "file");
+
+                if ($form->getElement('create_exhibit')->isChecked()) {
+                    $geo = @tei_editions_get_item_places($item);
+
+                    $exhibit = new NeatlineExhibit;
+                    $exhibit->title = metadata($item, 'display_title');
+                    // FIXME: Make slug safe...
+                    $exhibit->slug = metadata($item, 'display_title');
+                    $exhibit->spatial_layer = 'OpenStreetMap';
+                    $exhibit->save(true);
+
+                    foreach ($geo as $teiPlace) {
+                        $place = new NeatlineRecord;
+                        $place->exhibit_id = $exhibit->id;
+                        $place->title = $teiPlace["name"];
+                        $place->item_id = $item->id;
+                        // FIXME: Need to convert WGS84 to WKT...?
+                        $place->coverage = "Point(" . $teiPlace["longitude"] . " " . $teiPlace["latitude"] . ")";
+                        $place->save();
+                    }
+
+                }
+                $tx->commit();
+            } catch (Exception $e) {
+                error_log($e->getTraceAsString());
+                $tx->rollBack();
+                $this->_helper->_flashMessenger(
+                    __('There was an error on the form: ' . $e->getMessage()), 'error');
+                return;
+            }
 
             $this->_helper->flashMessenger(
                 __('The TEI was successfully imported!'), 'success');
