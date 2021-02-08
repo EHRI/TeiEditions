d34687d8df126db206fccd1d5e72cb5c8a883f03
Mike Bryant <mikesname@gmail.com>
2021-02-08 14:49:21 +0000

    Various changes for improved release process and deployment
     - added a licence file
     - added a Gruntfile for building a release archive .zip (shame
       this is seemingly necessary - can't do it easily w/ a shell
       script.)
     - renamed travis file and updated PHP version

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+
+/**
+ * @package     omeka
+ * @subpackage  neatline
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-compress');
+
+  var pkg = grunt.file.readJSON('package.json');
+
+  grunt.initConfig({
+
+    compress: {
+
+      dist: {
+        options: {
+          archive: 'pkg/TeiEditions-'+pkg.version+'.zip'
+        },
+        dest: 'TeiEditions/',
+        src: [
+
+          '**',
+
+          // GIT
+          '!.git/**',
+          '!.gitignore',
+
+          // NPM
+          '!package.json',
+          '!node_modules/**',
+
+          // COMPOSER
+          '!composer.json',
+          '!composer.lock',
+          '!vendor/**',
+
+          // GRUNT
+          '!.grunt/**',
+          '!Gruntfile.js',
+
+          // DIST
+          '!pkg/**',
+
+          // TESTS
+          '!test/**',
+
+          // CI
+          '!.travis.yml',
+
+          // Editor settings
+          '!*.vim',
+          '!.idea',
+          '!*.iml',
+        ]
+      }
+
+    }
+
+  });
+
+  // Spawn release package.
+  grunt.registerTask('package', [
+    'compress'
+  ]);
+};
