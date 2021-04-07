45d2df82113d59bf8f5cc9086d3e5049df726068
Mike Bryant <mikesname@gmail.com>
2021-04-07 17:38:15 +0100

    Add a batch running for the TEI enhancer endpoint
    This can handle a zip file. It also runs set_time_limit which,
    according to some vague documentation, extends the PHP script timeout
    from the standard value, hopefully avoiding a timeout on large
    batches.

---
+++ helpers/TeiEditions_BatchEnhancer.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * @package TeiEditions
+ *
+ * @copyright Copyright 2021 King's College London Department of Digital Humanities
+ */
+
+require_once __DIR__ . '/TeiEditions_DataFetcher.php';
+
+/**
+
+*/
+class TeiEditions_BatchEnhancer
+{
+
+    public function enhance($path, $mime, $dictPath, $lang, &$added): string
+    {
+        $dict = [];
+        if ($dictPath !== null) {
+            $doc = TeiEditions_DocumentProxy::fromUriOrPath($dictPath);
+            foreach ($doc->entities() as $entity) {
+                $dict[$entity->ref()] = $entity;
+            }
+        }
+
+        $src = new TeiEditions_DataFetcher($dict, $lang);
+        switch ($mime) {
+            case "text/xml":
+            case "application/xml":
+                $temp = tempnam(sys_get_temp_dir(), '');
+                if (($fh = fopen($temp, 'w')) !== false) {
+                    $doc = TeiEditions_DocumentProxy::fromUriOrPath($path);
+                    fwrite($fh, $this->enhanceFile($doc, $src, $added));
+                    fclose($fh);
+                } else {
+                    throw new Exception("Unable to open temp file for writing: $temp");
+                }
+                return $temp;
+            case "application/zip":
+                return $this->enhanceZip($path, $src, $added);
+            default:
+                throw new Exception("Unrecognised mimetype: $mime");
+        }
+    }
+
+    private function enhanceZip($path, TeiEditions_DataFetcher $src, &$added): string
+    {
+        $temp = tempnam(sys_get_temp_dir(), '');
+        $out = new ZipArchive;
+        if ($out->open($temp) !== false) {
+            $zip = new ZipArchive;
+            if ($zip->open($path) !== false) {
+                for ($i = 0; $i < $zip->count(); $i++) {
+                    $name = $zip->getNameIndex($i);
+                    $xml_str = $zip->getFromIndex($i);
+                    $doc = TeiEditions_DocumentProxy::fromString($xml_str);
+                    $out->addFromString($name, $this->enhanceFile($doc, $src, $added));
+
+                    // In theory this should extend the default script timeout
+                    // on every pass...
+                    set_time_limit(10);
+                }
+                $zip->close();
+            } else {
+                throw new Exception("Unable to open input zip file");
+            }
+            $out->close();
+        } else {
+            throw new Exception("Unable to create output zip file");
+        }
+
+        return $temp;
+    }
+
+    private function enhanceFile(TeiEditions_DocumentProxy $doc, TeiEditions_DataFetcher $src, &$added): string
+    {
+        $tool = new TeiEditions_TeiEnhancer($doc, $src);
+        $added += $tool->addReferences();
+        return $doc->document()->saveXML();
+    }
+}
